65d1b63444fd38a6eae68d618ee98b30cef7f09e
MotUika <motdapchai@gmail.com>
2025-10-02 23:49:30 +0700

    Add MajorDAO

---
+++ DataAccessLayer/DAO/MajorDAO.cs
@@ -0,0 +1,16 @@
+﻿using BussinessObject.Models;
+using DataAccessLayer.BaseDAO;
+using DataAccessLayer.IBaseDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DAO
+{
+    public class MajorDAO : BaseDAO<Major>, IBaseDAO<Major>
+    {
+        public MajorDAO(ASDPRSContext context) : base(context) { }
+    }
+}
