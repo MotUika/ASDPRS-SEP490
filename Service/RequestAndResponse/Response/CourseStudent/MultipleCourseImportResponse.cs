971a217528d3c4e5c22b54ea9ab6235367a16896
MotUika <motdapchai@gmail.com>
2025-10-10 20:49:35 +0700

    aad import student and multi import class student

---
+++ Service/RequestAndResponse/Response/CourseStudent/MultipleCourseImportResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Service.RequestAndResponse.Response.CourseStudent
+{
+    public class MultipleCourseImportResponse
+    {
+        public int TotalSuccessCount { get; set; }
+        public List<SheetImportResult> SheetResults { get; set; }
+    }
+
+    public class SheetImportResult
+    {
+        public string SheetName { get; set; }
+        public int? CourseInstanceId { get; set; }
+        public string CourseName { get; set; }
+        public int SuccessCount { get; set; }
+        public string Message { get; set; }
+        public List<CourseStudentResponse> ImportedStudents { get; set; }
+    }
+}
