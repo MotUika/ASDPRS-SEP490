6b2222cabeddccc09170efa53404c1290fead538
DannyVu24 <vumanhkhai2539@gmail.com>
2025-11-16 01:23:36 +0700

    regrade request for instructor

--- Service/Mapping/RegradeRequestProfile.cs
+++ Service/Mapping/RegradeRequestProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using BussinessObject.Models;
 using Service.RequestAndResponse.Response.RegradeRequest;
-using System.Linq;
 
 namespace Service.Mapping
 {
@@ -9,25 +8,38 @@ namespace Service.Mapping
     {
         public RegradeRequestProfile()
         {
+            // RegradeRequest -> RegradeRequestResponse
             CreateMap<RegradeRequest, RegradeRequestResponse>()
-                .ForMember(dest => dest.RequestedByStudent, opt => opt.MapFrom(src => src.Submission.User))
-                .ForMember(dest => dest.Assignment, opt => opt.MapFrom(src => src.Submission.Assignment))
-                .ForMember(dest => dest.ReviewedByInstructor, opt => opt.MapFrom(src => src.ReviewedByInstructor));
+                .ForMember(dest => dest.RequestedByStudent, opt => opt.MapFrom(src => new UserInfoRegradeResponse
+                {
+                    UserId = src.Submission.UserId,
+                    FullName = src.Submission.User != null ? $"{src.Submission.User.FirstName} {src.Submission.User.LastName}".Trim() : null,
+                    Email = src.Submission.User.Email
+                }))
+                .ForMember(dest => dest.ReviewedByInstructor, opt => opt.MapFrom(src => new UserInfoRegradeResponse
+                {
+                    UserId = src.ReviewedByInstructorId ?? 0,
+                    FullName = src.ReviewedByInstructor != null ? $"{src.ReviewedByInstructor.FirstName} {src.ReviewedByInstructor.LastName}".Trim() : null,
+                    Email = src.ReviewedByInstructor.Email
+                    
+                }))
+                .ForMember(dest => dest.Assignment, opt => opt.MapFrom(src => src.Submission.Assignment));
 
+            // Submission -> SubmissionInfoResponse
             CreateMap<Submission, SubmissionInfoResponse>();
 
+            // User -> UserInfoRegradeResponse (dùng cho mapping khác nếu cần)
             CreateMap<User, UserInfoRegradeResponse>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}".Trim()));
 
+            // Assignment -> AssignmentInfoRegradeResponse
             CreateMap<Assignment, AssignmentInfoRegradeResponse>()
                 .ForMember(
                     dest => dest.CourseName,
-                    opt => opt.MapFrom(src => src.CourseInstance != null 
-                                               && src.CourseInstance.Course != null
-                                               ? src.CourseInstance.Course.CourseName 
+                    opt => opt.MapFrom(src => src.CourseInstance != null && src.CourseInstance.Course != null
+                                               ? src.CourseInstance.Course.CourseName
                                                : null)
-                      );
-
+                );
         }
     }
 }
