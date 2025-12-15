669151922aa21dacf91ececc7fb35af784a61ee1
MotUika <motdapchai@gmail.com>
2025-12-15 21:30:47 +0700

    update remove key from app setting

--- Service/Service/GeminiAiService.cs
+++ Service/Service/GeminiAiService.cs
@@ -14,17 +14,15 @@ namespace Service.Service
     {
         private readonly HttpClient _httpClient;
         private readonly ILogger<GeminiAiService> _logger;
-        private readonly string _apiKey = "AIzaSyCjPB0vROznYBu7ffFqdaBVR58zhcUgRwc";
+        private readonly string _apiKey;
         private readonly string _baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/";
 
-        public GeminiAiService(HttpClient httpClient, ILogger<GeminiAiService> logger)
+        public GeminiAiService(HttpClient httpClient, ILogger<GeminiAiService> logger, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _logger = logger;
 
-            if (string.IsNullOrEmpty(_apiKey))
-            {
-                throw new ArgumentNullException("GoogleAI:ApiKey is missing in configuration");
-            }
+            _apiKey = configuration["Authentication:GoogleAI:ApiKey"] ?? throw new ArgumentNullException("Authentication:GoogleAI:ApiKey is missing in configuration");
         }
 
         public async Task<string> SummarizeAsync(string text, string model = null, int maxOutputTokens = 800)
